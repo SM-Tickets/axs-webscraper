5d3a853f5ddc955d6cbb4f9d7e3753e91862da51
Austin Liu <austinhliu@gmail.com>
2024-10-24 10:40:27 -0700

    fix: remove unneccesary code from pyinstaller hook

--- scripts/pyinstaller_hook.py
+++ scripts/pyinstaller_hook.py
@@ -1,11 +1,4 @@
 import os
-import subprocess
-
-subprocess.run(["playwright", "install"]) 
-# from playwright.__main__ import main as playwright_install
-#
-# if not os.path.exists(os.path.expanduser("~/.cache/ms-playwright")):
-#     playwright_install()
 
 # set PLAYWRIGHT_BROWSERS_PATH manually, since it is 0 by default when run with a pyinstaller binary (https://github.com/microsoft/playwright-python/pull/1002/files)
 HOME = os.environ["HOME"]
