7d43667955c817219838a90066b653d71ece972b
Austin Liu <austinhliu@gmail.com>
2025-03-31 13:03:36 -0700

    fix: package script
    Compressed archives now expand executables into the top level directory.

--- scripts/package.sh
+++ scripts/package.sh
@@ -19,17 +19,23 @@ fi
 mkdir package
 
 if [ "$(uname -o)" = "Msys" ]; then
-    zip -r axs-webscraper_win_x86-64.zip dist/axs-webscraper.exe browser_drivers/;
+    cp dist/axs-webscraper.exe .
+    zip -r axs-webscraper_win_x86-64.zip axs-webscraper.exe browser_drivers/;
+    rm axs-webscraper.exe
     cp dist/axs-webscraper.exe axs-webscraper-core_win_x86-64.exe
     mv axs-webscraper_win_x86-64.zip axs-webscraper-core_win_x86-64.exe package
 elif [ "$(uname -o)" = "Darwin" ]; then
     if [ "$(uname -m)" = "x86_64" ]; then
-        tar czvf axs-webscraper_macos_x86-64.tar.gz dist/axs-webscraper dist/axs-webscraper.app browser_drivers/;
+        cp -a dist/axs-webscraper dist/axs-webscraper.app .
+        tar czvf axs-webscraper_macos_x86-64.tar.gz axs-webscraper axs-webscraper.app browser_drivers/;
+        rm -rf axs-webscraper axs-webscraper.app
         cp dist/axs-webscraper axs-webscraper-core_macos_x86-64
         cp -a dist/axs-webscraper.app axs-webscraper-core_macos_x86-64.app
         mv axs-webscraper_macos_x86-64.tar.gz axs-webscraper-core_macos_x86-64 axs-webscraper-core_macos_x86-64.app package
     elif [ "$(uname -m)" = "arm64" ]; then
-        tar czvf axs-webscraper_macos_arm64.tar.gz dist/axs-webscraper dist/axs-webscraper.app browser_drivers/;
+        cp -a dist/axs-webscraper dist/axs-webscraper.app .
+        tar czvf axs-webscraper_macos_arm64.tar.gz axs-webscraper axs-webscraper.app browser_drivers/;
+        rm -rf axs-webscraper axs-webscraper.app
         cp dist/axs-webscraper axs-webscraper-core_macos_arm64
         cp -a dist/axs-webscraper.app axs-webscraper-core_macos_arm64.app
         mv axs-webscraper_macos_arm64.tar.gz axs-webscraper-core_macos_arm64 axs-webscraper-core_macos_arm64.app package
